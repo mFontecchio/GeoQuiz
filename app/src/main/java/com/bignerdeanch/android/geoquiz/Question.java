16679cb53809aa1a02cb482378ec0731c6c87cbc
Michael Fontecchio <mfonte3@live.spcollege.edu>
2018-03-12 21:47:23 -0400

    Updated GeoQuiz including challenges at the end of chapters 1 & 2. Application cycles through questions and has error handling to prevent out of bounds errors when working with array mCurrentIndex[].
    Signed-off-by: Michael Fontecchio <mfonte3@live.spcollege.edu>

---
+++ app/src/main/java/com/bignerdeanch/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdeanch.android.geoquiz;
+
+/**
+ * Created by Hayami on 3/12/2018.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
