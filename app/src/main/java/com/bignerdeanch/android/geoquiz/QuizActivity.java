fc77ec7acea2d36d7768b48be9697022fd1ed5c5
Michael Fontecchio <mfonte3@live.spcollege.edu>
2018-03-13 23:24:38 -0400

    Updated files following Chapter 3 tutorials and Chapter 3 Challenges. Fixed View reset on rotation/app killed by OS. Added basic handling to disable true/false buttons if the question was already answered. Added scoreQuiz method to tally up and display user score. ---- Minor cleanup to QuizActivity.java to better streamline navigation buttons so there is no overlapping code.
    Signed-off-by: Michael Fontecchio <mfonte3@live.spcollege.edu>

--- app/src/main/java/com/bignerdeanch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdeanch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdeanch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,12 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static java.lang.String.valueOf;
+
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -27,11 +33,20 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    //Initialize questionHistory with a value that is impossible for array Index to fill.
+    private int questionHistory = -1;
+    private int correctAnswers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -63,9 +78,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
-                //Ensure we do not get out of bounds errors when cycling.
-                if (mCurrentIndex >= 0 ) {
-                } else {
+                //Ensure we do not get index out of bounds errors when cycling.
+                if (mCurrentIndex < 0 ) {
                     mCurrentIndex = mQuestionBank.length - 1;
                 }
                 updateQuestion();
@@ -77,7 +91,13 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
+                if (mCurrentIndex == 0){
+                    scoreQuiz();
+                    updateQuestion();
+                } else {
+                    updateQuestion();
+                }
+
             }
         });
 
@@ -87,6 +107,8 @@ public class QuizActivity extends AppCompatActivity {
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -94,11 +116,61 @@ public class QuizActivity extends AppCompatActivity {
 
         int messageResID = 0;
 
-        if (userPressedTrue == answerIsTrue) {
-            messageResID = R.string.correct_toast;
+        if (questionHistory < mCurrentIndex) {
+            if (userPressedTrue == answerIsTrue) {
+                messageResID = R.string.correct_toast;
+                correctAnswers ++;
+            } else {
+                messageResID = R.string.incorrect_toast;
+            }
+            Toast.makeText(this, messageResID, Toast.LENGTH_SHORT).show();
+            mTrueButton.setEnabled(false);
+            mFalseButton.setEnabled(false);
         } else {
-            messageResID = R.string.incorrect_toast;
+            Toast.makeText(this, R.string.answered_question, Toast.LENGTH_SHORT).show();
         }
-        Toast.makeText(this, messageResID, Toast.LENGTH_SHORT).show();
+        questionHistory = mCurrentIndex;
+    }
+
+    private void scoreQuiz() {
+        float gradeTotal = ((float)correctAnswers / mQuestionBank.length) * 100;
+        Toast.makeText(this, String.format("Your score is: %3.2f ", gradeTotal), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
